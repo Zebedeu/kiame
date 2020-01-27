f16ab3b335ca5a4d5281aff0035b39cc97c76cc2
Zebedeu <marciozebedeu@gmail.com>
2020-01-27 18:35:00 +0100

    bug/fix-bug-class-not-found

--- footer.php
+++ footer.php
@@ -11,9 +11,11 @@
 		wp_nav_menu( array(
 			'theme_location'    => 'secondary',
 			'depth'             => 2,
-			'menu_class'        => 'nav navbar-nav',
-			'fallback_cb'       => 'MZ_Walker_Nav_Menu::fallback',
-			'walker'            => new MZ_Walker_Nav_Menu(),
+			'container'         => 'div',
+			'container_class'   => 'collapse navbar-collapse',
+			'container_id'      => 'navbarNav',
+			'fallback_cb'       => 'Kiame_Walker_Nav_Menu::fallback',
+			'walker'            => new Kiame_Walker_Nav_Menu(),
 		) );
 		?>
 
