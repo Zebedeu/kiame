7b0f0af0ee4d8b28cb9227cf389b36f93c622803
Zebedeu <marciozebedeu@gmail.com>
2020-01-30 23:39:25 +0100

    Update content-page.php

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -1,13 +1,12 @@
 
 <div id="post-<?php the_ID();?>" <?php  post_class("container") ?>>
     <div class="text-center" style="padding: 3vh;">
-        <h2><?php echo the_title_attribute( ); ?></h2>
     </div>
     <?php if( ! kiame_get_attachment() ): ?>
-    <div class="image" style="background-image:url(<?php echo esc_url(get_template_directory_uri()) . '/assets/img/tech/image4.jpg';?>);">
+    <div class="image card-header" style="background-image:url(<?php echo esc_url(get_template_directory_uri()) . '/assets/img/tech/image4.jpg';?>);">
     </div>
     <?php else: ?>
-    <div class="image" style="background-image:url(<?php echo esc_url(kiame_get_attachment()); ?>);"></div>
+    <div class="image card-header" style="background-image:url(<?php echo esc_url(kiame_get_attachment()); ?>);"></div>
     <?php endif;?>
     <div class="row">
                     <?php if( ! is_active_sidebar( 'sidebar-1' ) ): ?>
@@ -15,7 +14,7 @@
                         <?php else: ?>
                         <div class="col-12 col-md-8 info card-header">
                         <?php endif;?>
-            <?php the_title( '<h3 class="entry-title">', '</h3>'); ?>
+            <?php the_title( '<h2 class="entry-title card-title">', '</h2>'); ?>
             <p><?php the_content();?></p>
          </div>
                 <?php get_sidebar(); ?>
